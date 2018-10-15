da3fc6a3978221cb9fe7f28b9603677c794edbb9
skywind3000 <skywind3000@163.com>
2018-10-15 18:13:45 +0800

    remove files

--- apache2/apache2-svn/build.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-docker rmi skywind3000/apache2:svn 2> /dev/null
-docker build -t skywind3000/apache2:svn .
-
--- apache2/apache2-svn/build/init/run-apache2-svn.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-chown www-data:www-data /var/lib/svn
-chown www-data:www-data /var/lib/svn/*
-
-exec bash /usr/local/bin/run-apache2.sh
