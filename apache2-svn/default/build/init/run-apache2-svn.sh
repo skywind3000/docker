05f83a42147134907dae5bba1ab9d0adb52a2c61
skywind3000 <skywind3000@163.com>
2018-10-16 00:16:23 +0800

    update svn

--- apache2-svn/default/build/init/run-apache2-svn.sh
+++ apache2-svn/default/build/init/run-apache2-svn.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+[ -d /var/lib/svn ] || mkdir -p /var/lib/svn 2> /dev/null
+[ -d /var/lib/svn/conf ] || mkdir -p /var/lib/svn/conf 2> /dev/null
+[ -d /var/lib/svn/repos ] || mkdir -p /var/lib/svn/repos 2> /dev/null
+[ -d /var/lib/svn/history ] || mkdir -p /var/lib/svn/history 2> /dev/null
+
 chown www-data:www-data /var/lib/svn
 chown www-data:www-data /var/lib/svn/*
 
