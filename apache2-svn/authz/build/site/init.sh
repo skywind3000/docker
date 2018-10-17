ab3087a28991d97fe20ea93c62aa48450c4ec5e4
skywind3000 <skywind3000@163.com>
2018-10-18 01:31:09 +0800

    update svn

--- apache2-svn/authz/build/site/init.sh
+++ apache2-svn/authz/build/site/init.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 
+# initialize directories
 mkdir -p /var/lib/backup 2> /dev/null
 mkdir -p /var/lib/svn/logs 2> /dev/null
 
@@ -12,13 +13,15 @@ chown www-data:www-data /var/lib/svn/conf/davsvn.passwd
 
 rm -rf /etc/cron.d/*
 
+# install cron task for auto auth
 if [ -n "$SVN_AUTOAUTH" ]; then
 	SCRIPT='/var/lib/site/script/svnauth.py'
 	CONFIG='/var/lib/site/etc/svnauth.json'
 	FILE='/etc/cron.d/svnauth'
-	echo "0/$SVN_AUTOAUTH * * * *   www-data   /usr/bin/python $SCRIPT $CONFIG" > $FILE
+	echo "*/$SVN_AUTOAUTH * * * *   www-data   /usr/bin/python $SCRIPT $CONFIG" > $FILE
 fi
 
+# install cron task for backup
 if [ -n "$SVN_BACKUP" ]; then
 	FILE='/etc/cron.d/svnback'
 	echo "$SVN_BACKUP" > /var/lib/svn/conf/backup.txt
