c2b74d956741d68e70cc997a03602d0eeae5b73e
skywind3000 <skywind3000@gmail.com>
2018-11-18 03:55:26 +0800

    fixed password error

--- mysql/8.0/config/setup_password.sh
+++ mysql/8.0/config/setup_password.sh
@@ -1,5 +1,13 @@
 #! /bin/sh
 
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	if [ -z "$MYSQL_ROOT_PASSWORD" -a -z "$MYSQL_ALLOW_EMPTY_PASSWORD" -a -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
+		echo >&2 'error: database is uninitialized and password option is not specified '
+		echo >&2 '  You need to specify one of MYSQL_ROOT_PASSWORD, MYSQL_ALLOW_EMPTY_PASSWORD and MYSQL_RANDOM_ROOT_PASSWORD'
+		exit 1
+	fi
+fi
+
 if [ ! -z "$MYSQL_RANDOM_ROOT_PASSWORD" ]; then
 	export MYSQL_ROOT_PASSWORD="$(pwgen -1 32)"
 	echo "GENERATED ROOT PASSWORD: $MYSQL_ROOT_PASSWORD"
