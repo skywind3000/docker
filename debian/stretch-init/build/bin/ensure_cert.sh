2eec37214fad5596bd9f595a504290195a43002d
skywind3000 <skywind3000@gmail.com>
2018-11-18 13:14:27 +0800

    update

--- debian/stretch-init/build/bin/ensure_cert.sh
+++ debian/stretch-init/build/bin/ensure_cert.sh
@@ -1,14 +1,36 @@
 #! /bin/sh
 
-CERT_S="$1"
-CERT_K="$2"
 
-[ -n "$3" ] && CERT_N="$3" || CERT_N="unknow"
-[ -n "$4" ] && CERT_D="$4" || CERT_D="unknow"
+create_ssl_cert() {
+	openssl req -new -x509 -days 36500 -nodes -out "$1" -keyout "$2" \
+			-subj "/C=US/CN=unknow/O=unknow" 
+	if [ -e "$2" ]; then
+		chmod 640 "$2" 2> /dev/null
+		chown root:ssl-cert "$2" 2> /dev/null
+	 fi
+}
 
-if [ -f "$CERT_S" ] && [ -f "$CERT_K" ]; then
-	exit
-fi
+check_self() {
+	F1=/etc/ssl/self/ssl-cert.pem
+	F2=/etc/ssl/self/ssl-cert.key
+	[ -d /etc/ssl/self ] || mkdir -p /etc/ssl/self 2> /dev/null
+	if [ -e "$F1" ] && [ -e "$F2" ]; then
+		return
+	fi
+	create_ssl_cert "$F1" "$F2" 2> /dev/null
+}
 
-openssl req -new -x509 -days 36500 -nodes -out "$CERT_S" -keyout "$CERT_K" \
-	    -subj "/C=US/CN=$CERT_N/O=$CERT_D"
+check_snake() {
+	F1=/etc/ssl/certs/ssl-cert-snakeoil.pem
+	F2=/etc/ssl/private/ssl-cert-snakeoil.key
+	if [ -e "$F1" ] && [ -e "$F2" ]; then
+		return
+	fi
+	if [ -x "/usr/sbin/make-ssl-cert" ]; then
+		/usr/sbin/make-ssl-cert generate-default-snakeoil
+	fi
+}
+
+check_self
+check_snake
+
