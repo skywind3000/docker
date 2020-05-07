2f4c49c4907faad286c5557400808c866d69b19e
skywind3000 <skywind3000@gmail.com>
2020-05-08 03:30:00 +0800

    update apache2

--- nextcloud/default/build/init/nextcloud-update.sh
+++ nextcloud/default/build/init/nextcloud-update.sh
@@ -2,3 +2,5 @@
 
 cd /var/www/nextcloud
 sudo -u www-data php occ maintenance:update:htaccess
+touch /var/www/nextcloud/config/CAN_INSTALL
+chown www-data:www-data /var/www/nextcloud/config/CAN_INSTALL
