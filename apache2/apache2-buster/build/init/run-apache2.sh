88e7ad415c2f0a771d6e1f5ce5be0ca0bd995ca7
skywind3000 <skywind3000@gmail.com>
2020-05-12 15:47:24 +0800

    update

--- apache2/apache2-buster/build/init/run-apache2.sh
+++ apache2/apache2-buster/build/init/run-apache2.sh
@@ -8,25 +8,6 @@ fi
 # Apache gets grumpy about PID files pre-existing
 rm -f /var/run/apache2/apache2*.pid
 
-# change uid
-if [ -n "$PUID" ]; then
-	usermod -o -u "$PUID" www-data
-fi
-
-# change gid
-if [ -n "$PGID" ]; then
-	groupmod -o -g "$PGID" www-data
-fi
-
-# run memcached
-if [ -n "$MEMCACHED_ENABLE" ]; then
-	/usr/bin/memcached -d \
-		-m "${MEMCACHED_MEM:-64}" \
-		-p "${MEMCACHED_PORT:-11211}" \
-		-u "${MEMCACHED_USER:-www-data}" \
-		-l "${MEMCACHED_HOST:-127.0.0.1}"
-fi
-
 # run initialize scripts
 /bin/sh /usr/local/etc/initz.rc execute
 
